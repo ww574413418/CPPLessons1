508551257cd8e6b183aaa1a90a9a4136c2b9c8a2
Grubby <574413418@qq.com>
2022-07-05 10:12:44 +0800

    Initial Commit

---
+++ CPPLessons/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPLessons
+//
+//  Created by Grubby on 5/7/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
